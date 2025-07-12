56c00c04d9b354cc964dd42a7fd65b7d14c4cb43
KrishnaVamsibakka <bakkakrishnavamsireddy09@gmail.com>
2025-07-12 06:28:56 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -3,7 +3,7 @@ import os
 class Config(object):
   API_ID = int(os.environ.get("API_ID", "29055333"))
   API_HASH = os.environ.get("API_HASH", "a6d154242eaef80a163bf5d0a7763882")
-  BOT_TOKEN = os.environ.get("BOT_TOKEN", "7795414140:AAHPYxU-Yc7AJ3KcR-zWZmqFOYd4C_mNItQ")
+  BOT_TOKEN = os.environ.get("BOT_TOKEN", "8178307547:AAHx8tkXwi79zmWY3ONk5GOaeV61Dm1i_aM")
   BOT_USERNAME = os.environ.get("BOT_USERNAME", "Cartoonworldtelugu_india_bot")
   DB_CHANNEL = int(os.environ.get("DB_CHANNEL", "-1002526508320"))
   SHORTLINK_URL = os.environ.get('SHORTLINK_URL', "Gplinks.com")
@@ -11,7 +11,7 @@ class Config(object):
   BOT_OWNER = int(os.environ.get("BOT_OWNER", "6828129421"))
   DATABASE_URL = os.environ.get("DATABASE_URL", "mongodb+srv://anjireddyb98:3qiaVJINXarqFNKp@cluster0.kohnpcd.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
   UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", "-1002024495069")
-  LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1002556802708"))
+  LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", ""))
   BANNED_USERS = set(int(x) for x in os.environ.get("BANNED_USERS", "").split())
   FORWARD_AS_COPY = bool(os.environ.get("FORWARD_AS_COPY", True))
   BROADCAST_AS_COPY = bool(os.environ.get("BROADCAST_AS_COPY", True))
