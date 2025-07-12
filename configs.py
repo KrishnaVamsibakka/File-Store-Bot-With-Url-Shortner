94938cec238617bb2bef4094d7bfe7372f659d13
KrishnaVamsibakka <bakkakrishnavamsireddy09@gmail.com>
2025-07-12 06:31:10 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -11,14 +11,14 @@ class Config(object):
   BOT_OWNER = int(os.environ.get("BOT_OWNER", "6828129421"))
   DATABASE_URL = os.environ.get("DATABASE_URL", "mongodb+srv://anjireddyb98:3qiaVJINXarqFNKp@cluster0.kohnpcd.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
   UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", "-1002024495069")
-  LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", ""))
+  LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1002630777690"))
   BANNED_USERS = set(int(x) for x in os.environ.get("BANNED_USERS", "").split())
   FORWARD_AS_COPY = bool(os.environ.get("FORWARD_AS_COPY", True))
   BROADCAST_AS_COPY = bool(os.environ.get("BROADCAST_AS_COPY", True))
   BANNED_CHAT_IDS = list(set(int(x) for x in os.environ.get("BANNED_CHAT_IDS", "").split()))
   OTHER_USERS_CAN_SAVE_FILE = bool(os.environ.get("OTHER_USERS_CAN_SAVE_FILE", False))
   ABOUT_BOT_TEXT = f"""
-This is a Permanent FileStore Bot Created by 𝐕𝐚𝐦𝐬𝐢 🐦‍🔥.Others Can use me by joining my updates channel
+This is a Permanent FileStore Bot Created by 𝐕𝐚𝐦𝐬𝐢 🐦‍🔥.I will give files of my updates channel.
 
 ╭────[ 🔅FɪʟᴇSᴛᴏʀᴇBᴏᴛ🔅]────⍟
 │
